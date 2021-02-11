4836af28abec7878e22c720c45157b56e2f5160b
csarraud <clementsarraud@gmail.com>
2021-02-11 17:53:05 +0100

    add chakra regeneration nbt

--- src/main/java/fr/sonkuun/becameashinobi/capability/ChakraData.java
+++ src/main/java/fr/sonkuun/becameashinobi/capability/ChakraData.java
@@ -9,10 +9,12 @@ public class ChakraData {
 
 	private double chakraMaxValue;
 	private double chakraValue;
+	private double chakraRegenerationPerSecond;
 
 	public ChakraData() {
 		this.chakraMaxValue = 0;
 		this.chakraValue = 0;
+		this.chakraRegenerationPerSecond = 0;
 	}
 	
 	public void setChakraValues(double maxValue, double value) {
@@ -20,8 +22,13 @@ public class ChakraData {
 		this.chakraValue = value;
 	}
 	
+	public void setChakraRegenerationPerSecond(double value) {
+		this.chakraRegenerationPerSecond = value;
+	}
+	
 	public static final String CHAKRA_MAX_VALUE_NBT = "chakra_max_value";
 	public static final String CHAKRA_VALUE_NBT = "chakra_value";
+	public static final String CHAKRA_REGENERATION_PER_SECOND_NBT = "chakra_regeneration_per_second";
 
 	public static class ShinobiWeaponDataNBTStorage implements Capability.IStorage<ChakraData> {
 
@@ -31,6 +38,7 @@ public class ChakraData {
 
 			tag.putDouble(CHAKRA_MAX_VALUE_NBT, instance.chakraMaxValue);
 			tag.putDouble(CHAKRA_VALUE_NBT, instance.chakraValue);
+			tag.putDouble(CHAKRA_REGENERATION_PER_SECOND_NBT, instance.chakraRegenerationPerSecond);
 
 			return tag;
 		}
@@ -44,6 +52,7 @@ public class ChakraData {
 				instance.setChakraValues(
 						tag.getDouble(CHAKRA_MAX_VALUE_NBT),
 						tag.getDouble(CHAKRA_VALUE_NBT));
+				instance.setChakraRegenerationPerSecond(tag.getDouble(CHAKRA_REGENERATION_PER_SECOND_NBT));
 			}
 		}
 
