c4e93c8940cc805e8d5b9881ef3b73d625fedabd
csarraud <clementsarraud@gmail.com>
2021-02-18 17:50:19 +0100

    update value only on server side

--- src/main/java/fr/sonkuun/becameashinobi/listener/BecameAShinobiListener.java
+++ src/main/java/fr/sonkuun/becameashinobi/listener/BecameAShinobiListener.java
@@ -5,11 +5,16 @@ import fr.sonkuun.becameashinobi.capability.ChakraData;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
 
 public class BecameAShinobiListener {
 	
 	@SubscribeEvent
 	public void playerTick(PlayerTickEvent event) {
+		if(event.side.equals(LogicalSide.CLIENT)) {
+			return;
+		}
+		
 		PlayerEntity player = event.player;
 		
 		if(!player.getCapability(CapabilityBecameAShinobi.CAPABILITY_BECAME_A_SHINOBI).isPresent()) {
