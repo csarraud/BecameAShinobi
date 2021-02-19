6b9909f12e62b8867cc4559b4725aab9e61bcae1
csarraud <clementsarraud@gmail.com>
2021-02-19 10:30:10 +0100

    add event for debug purpose

--- src/main/java/fr/sonkuun/becameashinobi/listener/BecameAShinobiListener.java
+++ src/main/java/fr/sonkuun/becameashinobi/listener/BecameAShinobiListener.java
@@ -2,12 +2,31 @@ package fr.sonkuun.becameashinobi.listener;
 
 import fr.sonkuun.becameashinobi.capability.CapabilityBecameAShinobi;
 import fr.sonkuun.becameashinobi.capability.ChakraData;
+import fr.sonkuun.becameashinobi.network.BecameAShinobiPacketHandler;
+import fr.sonkuun.becameashinobi.network.MyPacket;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.network.PacketDistributor;
 
 public class BecameAShinobiListener {
+	
+	/*
+	 * Used for debug purpose only
+	 */
+	@SubscribeEvent
+	public void interact(PlayerInteractEvent event) {
+		if(event.getPlayer().getCapability(CapabilityBecameAShinobi.CAPABILITY_BECAME_A_SHINOBI).isPresent()) {
+			ChakraData data = event.getPlayer().getCapability(CapabilityBecameAShinobi.CAPABILITY_BECAME_A_SHINOBI).orElse(null);
+			data.useChakra(10);
+			PlayerEntity player = event.getPlayer();
+			BecameAShinobiPacketHandler.INSTANCE.send(PacketDistributor.SERVER.noArg(), new MyPacket(player.getUniqueID(), data));
+		}
+	}
 	
 	@SubscribeEvent
 	public void playerTick(PlayerTickEvent event) {
@@ -24,5 +43,6 @@ public class BecameAShinobiListener {
 		ChakraData chakraData = player.getCapability(CapabilityBecameAShinobi.CAPABILITY_BECAME_A_SHINOBI).orElse(null);
 		
 		chakraData.updateChakra();
+		BecameAShinobiPacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) player), new MyPacket(player.getUniqueID(), chakraData));
 	}
 }
