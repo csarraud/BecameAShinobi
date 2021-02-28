2ebad0c0a359b98b67587db4038cc22c8da096ea
csarraud <clementsarraud@gmail.com>
2021-02-28 20:08:42 +0100

    add doton widget

--- src/main/java/fr/sonkuun/becameashinobi/gui/ChakraSkillGui.java
+++ src/main/java/fr/sonkuun/becameashinobi/gui/ChakraSkillGui.java
@@ -1,5 +1,6 @@
 package fr.sonkuun.becameashinobi.gui;
 
+import fr.sonkuun.becameashinobi.gui.widget.DotonSkillTreeWidget;
 import fr.sonkuun.becameashinobi.gui.widget.FutonSkillTreeWidget;
 import fr.sonkuun.becameashinobi.gui.widget.JutsuTreeGuiWidget;
 import fr.sonkuun.becameashinobi.gui.widget.KatonSkillTreeWidget;
@@ -20,7 +21,7 @@ public class ChakraSkillGui extends AbstractJutsuGui {
 		this.addButton(new SuitonSkillTreeWidget(200, 150, "SUITON"));
 		this.addButton(new FutonSkillTreeWidget(300, 150, "FUTON"));
 		this.addButton(new RaitonSkillTreeWidget(280, 200, "RAITON"));
-		this.addButton(new JutsuTreeGuiWidget(230, 200, "DOTON"));
+		this.addButton(new DotonSkillTreeWidget(230, 200, "DOTON"));
 		
 		super.init();
 		
---
+++ src/main/java/fr/sonkuun/becameashinobi/gui/widget/DotonSkillTreeWidget.java
@@ -0,0 +1,38 @@
+package fr.sonkuun.becameashinobi.gui.widget;
+
+import fr.sonkuun.becameashinobi.BecameAShinobi;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.ResourceLocation;
+
+public class DotonSkillTreeWidget extends AbstractSkillTreeWidget {
+
+	public DotonSkillTreeWidget(int xIn, int yIn, String msg) {
+		super(xIn, yIn, msg);
+	}
+
+	@Override
+	public ResourceLocation getTexture() {
+		return new ResourceLocation(BecameAShinobi.MODID, "textures/gui/element.png");
+	}
+
+	@Override
+	public int getOffsetX() {
+		return 64;
+	}
+
+	@Override
+	public int getOffsetY() {
+		return 0;
+	}
+
+	@Override
+	public String getScreenName() {
+		return "Doton Skill Tree";
+	}
+
+	@Override
+	public Screen getGuiToDisplay() {
+		return null;
+	}
+
+}
