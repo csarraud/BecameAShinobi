5b7fdd33d4656d203c173b522caacec77136880d
csarraud <clementsarraud@gmail.com>
2021-05-02 18:50:26 +0200

    add capabilities to living entities

--- src/main/java/fr/sonkuun/becameashinobi/capability/CapabilityProviderEntities.java
+++ src/main/java/fr/sonkuun/becameashinobi/capability/CapabilityProviderEntities.java
@@ -9,14 +9,13 @@ import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 
-public class CapabilityProviderPlayers implements ICapabilitySerializable<INBT> {
+public class CapabilityProviderEntities implements ICapabilitySerializable<INBT> {
 
 	private final Direction NO_SPECIFIC_SIDE = null;
 
 	private ChakraData chakraData = new ChakraData();
 	private HealthData healthData = new HealthData();
-	private ElementalNatureData elementalNatureData = new ElementalNatureData();
-
+ 
 	@Nonnull
 	@Override
 	public <T> LazyOptional<T> getCapability(Capability<T> capability, Direction side) {
@@ -26,16 +25,12 @@ public class CapabilityProviderPlayers implements ICapabilitySerializable<INBT> 
 		else if(CapabilityBecameAShinobi.CAPABILITY_HEALTH == capability) {
 			return (LazyOptional<T>)LazyOptional.of(()-> healthData);
 		}
-		else if(CapabilityBecameAShinobi.CAPABILITY_ELEMENTAL_NATURE == capability) {
-			return (LazyOptional<T>)LazyOptional.of(()-> elementalNatureData);
-		}
 
 		return LazyOptional.empty();
 	}
 
 	private static final String CHAKRA_DATA_NBT = "chakra_data";
 	private static final String HEALTH_DATA_NBT = "health_data";
-	private static final String ELEMENTAL_NATURE_NBT = "elemental_nature";
 
 	@Override
 	public INBT serializeNBT() {
@@ -46,9 +41,6 @@ public class CapabilityProviderPlayers implements ICapabilitySerializable<INBT> 
 
 		INBT healthDataNBT = CapabilityBecameAShinobi.CAPABILITY_HEALTH.writeNBT(healthData, NO_SPECIFIC_SIDE);
 		nbt.put(HEALTH_DATA_NBT, healthDataNBT);
-		
-		INBT elementalNatureDataNBT = CapabilityBecameAShinobi.CAPABILITY_ELEMENTAL_NATURE.writeNBT(elementalNatureData, NO_SPECIFIC_SIDE);
-		nbt.put(ELEMENTAL_NATURE_NBT, elementalNatureDataNBT);
 		
 		return nbt;
 	}
@@ -62,9 +54,6 @@ public class CapabilityProviderPlayers implements ICapabilitySerializable<INBT> 
 
 		INBT healthDataNBT = tag.get(HEALTH_DATA_NBT);
 		CapabilityBecameAShinobi.CAPABILITY_HEALTH.readNBT(healthData, NO_SPECIFIC_SIDE, healthDataNBT);
-		
-		INBT elementalNatureDataNBT = tag.get(ELEMENTAL_NATURE_NBT);
-		CapabilityBecameAShinobi.CAPABILITY_ELEMENTAL_NATURE.readNBT(elementalNatureData, NO_SPECIFIC_SIDE, elementalNatureDataNBT);
 	}
-
+	
 }
